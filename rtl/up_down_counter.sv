cff93575f935959bda39d03a3df34c0d5ae51a44
BachNguyen2004 <bacnguyen@student.unimelb.edu.au>
2026-05-09 13:17:59 +0000

    additional

--- rtl/up_down_counter.sv
+++ rtl/up_down_counter.sv
@@ -7,17 +7,18 @@ module up_down_counter #(
     input logic clk,
     input logic enable,
     input logic up,
-    output logic [WIDTH-1:0] count
+    output logic [WIDTH-1:0] count     //Start at 0 represent using 2 bits -> so 00
 );
   // Width-correct constants for lint cleanliness
   localparam logic [WIDTH-1:0] Max = WIDTH'(MAX);
   localparam logic [WIDTH-1:0] One = WIDTH'(1);
 
   // Next-state signal
-  logic [WIDTH-1:0] next_count;
+  logic [WIDTH-1:0] next_count;  //next count will be 2 bits length
 
   // Initialise counter to 0
-  initial count = '0;
+  initial count = '0;  //' to make sure the initial count = 0 
+                       //will have exact number of bits you specified in the module
 
   // Next-state logic
   always_comb begin   //pure combinational logic (no memory, no clock, continously calculates next count)
