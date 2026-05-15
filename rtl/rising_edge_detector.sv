12912c1e412d975796db20a76e6ef8092d82ec54
BachNguyen2004 <bacnguyen@student.unimelb.edu.au>
2026-05-15 06:31:09 +0000

    Finish rising_edge_detector

--- rtl/rising_edge_detector.sv
+++ rtl/rising_edge_detector.sv
@@ -1,17 +1,15 @@
 `timescale 1ns / 1ps
 
 module rising_edge_detector (
-
     input  logic clk,
     input  logic sig_in,
     output logic rise
-
 );
 
-  logic prev = 1'b0;
-  always_ff @(posedge clk) begin
-    rise <= sig_in && !prev;
-    prev <= sig_in;
-  end
+  logic sig_prev = 1'b0;
+
+  always_ff @(posedge clk) sig_prev <= sig_in;
+
+  assign rise = sig_in && !sig_prev;
 
 endmodule
