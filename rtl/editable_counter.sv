2a4f4578844e9df74c3434c6667b5face7fff7ae
BachNguyen2004 <bacnguyen@student.unimelb.edu.au>
2026-05-17 12:26:44 +0000

    Complete editable counter

---
+++ rtl/editable_counter.sv
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+
+module editable_counter #(
+    parameter int N = 60,
+    parameter int WIDTH = 6
+) (
+    input  logic             clk,
+    input  logic             tick,       // Count increments on tick when edit_mode is low
+    input  logic             edit_mode,
+    input  logic             inc,        // Count increments by one when edit_mode is high
+    input  logic             dec,        // Count decrements by one when edit_mode is high
+    output logic [WIDTH-1:0] count
+);
+
+  logic enable;
+  logic up;
+
+  //internal counter
+  up_down_counter #(
+      .MAX  (N - 1),
+      .WIDTH(WIDTH)
+  ) u_counter (
+      .clk   (clk),
+      .enable(enable),
+      .up    (up),
+      .count (count)
+  );
+
+  wire inc_event = edit_mode && inc && !dec;  //Only happend in edit mode, 
+  wire dec_event = edit_mode && dec && !inc;  //
+  wire tick_event = !edit_mode && tick;  //Only happend in normal mode, every tick increment the counter
+
+  assign up = tick_event || inc_event;
+  assign enable = tick_event || inc_event || dec_event;
+
+endmodule
